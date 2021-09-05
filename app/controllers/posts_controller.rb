9b681f8233e1590de0db01647efc31b620b15cac
Amadou <amadou140589@gmail.com>
2021-09-05 12:38:01 +0100

    created action added in post controller. private methods set_post and post_params implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: %i[ show edit update destroy ]
+
     def index
         @posts = Post.all
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+    
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to posts_path, notice: "Post was successfully created." }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private
+    def set_post
+      @blog = Blog.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:content)
     end
 end
